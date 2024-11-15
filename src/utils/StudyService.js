fae7c0b0f084d5c3aff2a19e868b9d1136796826
Yaakoubi Mohamed <mohamedyaakoubiweb@gmail.com>
2024-11-15 13:36:33 +0100

    main

---
+++ src/utils/StudyService.js
@@ -0,0 +1,63 @@
+import Interceptor from "./axios-utils";
+
+export const getStudyById = (id) => {
+    return new Promise((resolve, reject) => {
+        Interceptor({
+            url: "/radiography/study/" + id,
+            method: 'GET',
+        })
+            .then(response => {
+                resolve(response.data);
+            })
+            .catch(error => {
+                reject(error);
+            });
+    });
+};
+
+// whre the obj (img) is directly associed to a study
+export const getObjPathByStudyId = (id) => {
+    return new Promise((resolve, reject) => {
+        Interceptor({
+            url: "/radiography/study/" + id,
+            method: 'GET',
+        })
+            .then(response => {
+                console.log(response.data)
+                resolve(response.data);
+            })
+            .catch(error => {
+                reject(error);
+            });
+    });
+};
+
+export const getSeriesByStudyKy = (id) => {
+    return new Promise((resolve, reject) => {
+        Interceptor({
+            url: "/radiography/study/allSeries/" + id,
+            method: 'GET',
+        })
+            .then(response => {
+                resolve(response.data);
+            })
+            .catch(error => {
+                reject(error);
+            });
+    });
+};
+
+export const getSeriesObjPathBySeriesKy = (id) => {
+    return new Promise((resolve, reject) => {
+        Interceptor({
+            url: "/radiography/series/object-paths/" + id,
+            method: 'GET',
+        })
+            .then(response => {
+                resolve(response.data);
+            })
+            .catch(error => {
+                reject(error);
+            });
+    });
+};
